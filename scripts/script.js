d30c4895eb4b1b3e97707065e06923c921539520
Stephen Liu <shl21@ic.ac.uk>
2024-08-15 16:11:03 +0100

    When all beam properties specified correctly, beam vis is triggered

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const BEAMLENGTHMESSAGE = document.getElementById('beam-length-error');
 const INERTIAMESSAGE = document.getElementById('moment-of-inertia-error'); 
 const MODULUSMESSAGE = document.getElementById('modulus-of-elasticity-error'); 
+const VISUALISATIONMESSAGE = document.getElementById('visualisation-block')
 
 const beamLengthInput = document.getElementById('beam-length');
 const beamLengthUnitsInput = document.getElementById('beam-length-unit');
@@ -19,36 +20,47 @@ var inertiaUnit;
 var modulus;
 var modulusUnit;
 
+var lengthValidated = false;
+var inertiaValidated = false;
+var modulusValidated = false;
 
 //validate inputs, outputting error message if invalid
 beamLengthInput.addEventListener('input', function() {  
     let errorMessage = validateBeamLength(beamLengthInput.value);
     BEAMLENGTHMESSAGE.textContent = errorMessage;
+
+    triggerVisualisation(lengthValidated,inertiaValidated,modulusValidated);
+
 });
 
 beamLengthUnitsInput.addEventListener('change', function() {
     let errorMessage = validateBeamLength(beamLengthInput.value);
     BEAMLENGTHMESSAGE.textContent = errorMessage;
+    triggerVisualisation(lengthValidated,inertiaValidated,modulusValidated);
 });
 
 inertiaInput.addEventListener('input', function() {  
     let errorMessage = validateInertia(inertiaInput.value);
     INERTIAMESSAGE.textContent = errorMessage;
+    triggerVisualisation(lengthValidated,inertiaValidated,modulusValidated);
 });
 
 inertiaUnitsInput.addEventListener('change', function() {
     let errorMessage = validateInertia(inertiaInput.value);
     INERTIAMESSAGE.textContent = errorMessage;
+    triggerVisualisation(lengthValidated,inertiaValidated,modulusValidated);
 });
 
 modulusInput.addEventListener('input', function() {  
     let errorMessage = validateModulus(modulusInput.value);
     MODULUSMESSAGE.textContent = errorMessage;
+    triggerVisualisation(lengthValidated,inertiaValidated,modulusValidated);
 });
 
 modulusUnitsInput.addEventListener('change', function() {
     let errorMessage = validateModulus(modulusInput.value);
     MODULUSMESSAGE.textContent = errorMessage;
+    triggerVisualisation(lengthValidated,inertiaValidated,modulusValidated);
 });
 
 
@@ -56,44 +68,66 @@ modulusUnitsInput.addEventListener('change', function() {
 function validateBeamLength(lengthInput) {
 
     if(lengthInput == "") {
+        lengthValidated = false;
         return "Beam length cannot be empty";
     }
     else if(lengthInput <= 0) {
+        lengthValidated = false;
         return "Beam length must be positive";
     }
     else {
+        lengthValidated = true;
         beamLength = lengthInput;
         beamLengthUnit = beamLengthUnitsInput.value;
-        return `Beam Length is ${beamLength} ${beamLengthUnit}`;
+        //return `Beam Length is ${beamLength} ${beamLengthUnit}`;
+        return "";
     }
 }
 
 function validateInertia(inertiaInput) {
 
     if(inertiaInput == "") {
+        inertiaValidated = false;
         return "Beam length cannot be empty";
     }
     else if(inertiaInput <= 0) {
+        inertiaValidated = false;
         return "Beam length must be positive";
     }
     else {
+        inertiaValidated = true;
         inertia = inertiaInput;
         inertiaUnit = inertiaUnitsInput.value;
-        return `Beam Length is ${inertia} ${inertiaUnit}`;
+        //return `Beam Length is ${inertia} ${inertiaUnit}`;
+        return ""; 
     }
 }
 
 function validateModulus(modulusInput) {
 
     if(modulusInput == "") {
+        modulusValidated = false;
         return "Young's Modulus cannot be empty";
     }
     else if(modulusInput <= 0) {
+        modulusValidated = false;
         return "Young's Modulus must be positive";
     }
     else {
+        modulusValidated = true;
         modulus = modulusInput;
         modulusUnit = modulusUnitsInput.value;
-        return `Young's Modulus is ${modulus} ${modulusUnit}`;
+        //return `Young's Modulus is ${modulus} ${modulusUnit}`;
+        return ""; 
+    }
+}
+
+function triggerVisualisation(validLengthInput,validateInertiaInput,validateModulusInput) {
+    
+    if(validLengthInput && validateInertiaInput && validateModulusInput){
+        VISUALISATIONMESSAGE.textContent = "READY";
+    }
+    else {
+        VISUALISATIONMESSAGE.textContent = "";
     }
 }
