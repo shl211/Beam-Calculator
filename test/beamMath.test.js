56eff06d8fd772420ffd9178f38c61e6866fc8f9
Stephen Liu <shl21@ic.ac.uk>
2024-08-17 13:16:38 +0100

    Edit test for simplifying list, to account for negatives

--- test/beamMath.test.js
+++ test/beamMath.test.js
@@ -66,16 +66,16 @@ describe("Beam Maths Library", function() {
         let func1 = new SingularityFunction(3,1,2);//<x-3>^2
         let func2 = new SingularityFunction(2,2,1);//2<x-2>
         let func3 = new SingularityFunction(2,0,4);//0
-        let func4 = new SingularityFunction(2,-0.5,1);//-0.5<x-2>
+        let func4 = new SingularityFunction(2,-3.5,1);//-4.5<x-2>
         let func5 = new SingularityFunction(4,1,2);//<x-4>^2
 
         let funcList = [func1,func2,func3,func4,func5];
 
         BeamMath.simplifySingularityFuncList(funcList);
 
-        //expect result to contain in any order: <x-3>^2, 1.5<x-2>, <x-4>^2
+        //expect result to contain in any order: <x-3>^2, -2.5<x-2>, <x-4>^2
         let res1 = new SingularityFunction(3,1,2);
-        let res2 = new SingularityFunction(2,1.5,1);
+        let res2 = new SingularityFunction(2,-2.5,1);
         let res3 = new SingularityFunction(4,1,2);
         let correct1 = false; //store states whethert each result has been verified
         let correct2 = false;
